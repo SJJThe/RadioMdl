c1db0f3a8e737bcc21fcd733412c49644336e65b
Samuel Thé <49229050+SJJThe@users.noreply.github.com>
2026-06-22 14:02:08 -0400

    correctly propagate variance in Cas A model

--- src/astro_mdl.jl
+++ src/astro_mdl.jl
@@ -67,39 +67,41 @@ estimates the flux of Cas A, given a frequency. Based on Baars et al. 2014
 function estim_casA_flux(center_freq::T;
     year::Int = Year(today()).value) where T
     
+    log10freq_MHz = log10(center_freq*1e-6)
+    log10freq_GHz = log10(center_freq*1e-9)
+
     if 22e6 < center_freq < 300e6
-        a = 5.625
-        var_a = .021^2
-        b = -.634
-        var_b = .015^2
-        c = -.023
-        var_c = .001^2
+        a, var_a = 5.625, .021^2
+        b, var_b = -.634, .015^2
+        c, var_c = -.023, .001^2
     elseif 300e6 < center_freq
         if center_freq > 31e9
             @warn "the model is not valid for frequencies above 31GHz"
         end
-        a = 5.880
-        var_a = .025^2
-        b = -0.792
-        var_b = .007^2
-        c = 0.
-        var_c = 0.
+        a, var_a = 5.880, .025^2
+        b, var_b = -0.792, .007^2
+        c, var_c = 0., 0.
     end
     
     # decay
-    decay = 0.97 - 0.3*log10(center_freq*1e-9) # in %/year since 1980
-    var_decay = .04^2 + .04^2*log10(center_freq*1e-9)^2
+    decay = 0.97 - 0.3*log10freq_GHz # in %/year since 1980
+    var_decay = .04^2 + .04^2*log10freq_GHz^2
     
     # log flux
-    log_S_Jy = a + b*log10(center_freq*1e-6) + c*log10(center_freq*1e-6)^2
-    var_log_S_Jy = var_a + var_b*log10(center_freq*1e-6)^2 + 
-                   var_c*log10(center_freq*1e-6)^4
+    log_S_Jy = a + b*log10freq_MHz + c*log10freq_MHz^2
+    var_log_S_Jy = var_a + var_b*log10freq_MHz^2 + var_c*log10freq_MHz^4
 
-    # flux in Jy
-    S_Jy = 10^log_S_Jy * (1 - decay*(year-1980)/100)
-    var_S_Jy = var_log_S_Jy / S_Jy^2 * (1 - decay*(year-1980)/100)^2 + 
-               10^log_S_Jy * var_decay 
-    
+    # constant flux in Jy
+    S0 = 10^log_S_Jy
+    var_S0 = (log(10) * S0)^2 * var_log_S_Jy
+
+    # decayed flux in Jy
+    k = (year - 1980) / 100
+    f = 1 - decay * k
+    var_f = k^2 * var_decay
+    S_Jy = S0 * f
+    var_S_Jy = f^2 * var_S0 + S0^2 * var_f
+
     return S_Jy, var_S_Jy
 end
 
@@ -150,7 +152,7 @@ function ground_model(alpha_grid::AbstractVector{T},
     beta_grid::AbstractVector{T},
     T_ground::AbstractMatrix{T}) where T
 
-    @assert size(mask) == (length(beta_grid), length(alpha_grid))
+    @assert size(T_ground) == (length(beta_grid), length(alpha_grid))
 
     return SphereMap(alpha_grid, beta_grid, T_ground)
 end
@@ -250,7 +252,7 @@ Yields a 'SphereMap' structure representing an atmosphere model with constant
 temperature 'T_eff' and opacity 'zenith_opacity'. The default alpha and beta grids
 are sampled at 1 degree resolution.
 
-"""
+"""#TODO: use Chapman function for better model at low elevation angles
 function atmosphere_model(alpha_grid::AbstractVector{T},
     beta_grid::AbstractVector{T},
     T_eff::T,
